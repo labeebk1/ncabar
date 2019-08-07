15e207e5df7ae6e604f7ae7ce12b557f0d68408d
Labeeb Khan <labeebk@Labeebs-MacBook-Pro.local>
2019-08-07 03:14:43 -0400

    added materialize and created login UI

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,9 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import M from 'materialize-css'
 
-class LoginPage extends React.Component {
-    render() {
-      return (<h1>You are logged out</h1>);
-    }
+class LoginPage extends Component {
+    
+
+  // constructor(props) {
+  //   super(props);
+  //   this.props.getLoginStatus();
+  // }
+  
+  componentDidMount(){
+    M.updateTextFields();
   }
+  render() {
+    return (
+      <div className="LoginPage">
+        <div className="valign-wrapper row login-box">
+        <div className="col card hoverable s10 pull-s1 m6 pull-m3 l4 pull-l4">
+          <div className="card-content">
+            <span className="card-title center-align green-text text-darken-3">
+              <i className="material-icons prefix">check_circle</i> NCA & Bar Login
+            </span>
+            <div className="row">
+              <form>
+              <div className="input-field col s12">
+                <i className="material-icons prefix">account_circle</i>
+                <input id="email" type="email" className="validate" autoComplete="on"/>
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="input-field col s12">
+                <i className="material-icons prefix">dialpad</i>
+                <input id="password" type="password" className="validate" autoComplete="on"/>
+                <label htmlFor="password">Password</label>
+              </div>
+              </form>
+            </div>
+          </div>
+          <div className="card-action right-align">
+            <button className="btn-flat grey-text waves-effect" type="submit">
+              Register
+            </button>
+            <button className="btn green waves-effect waves-light" type="submit">
+              Login
+            </button>
+          </div>
+        </div>
+        </div>
+        <footer className="page-footer green lighten-1 center-align">
+            <div className="container">
+            © 2019 Copyright NCA & Bar Tutoring Group
+            </div>
+        </footer>
+      </div>
+    )}
+}
 
-export default LoginPage;
+const mapStateToProps = state => ({
+  ...state
+})
+
+const mapDispatchToProps = {
+}
+  
+export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
